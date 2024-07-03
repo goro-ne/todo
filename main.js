548e4b537c02e6d0a8c63784051d13decc3081d0
goro-ne <hayao56@gmail.com>
2024-07-03 12:18:34 +0900

    Refactoring:未完了リスト

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ const onClickAdd = () => {
   // テキストボックスの内容を取得して初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-
+  // 未完了リストに追加
+  createIncompleteTodo(inputText);
+};
+/**
+ * 未完了リストに追加
+ * @param {*} todo string
+ */
+const createIncompleteTodo = (todo) => {
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
