83f6a96c1fc65080995c677d41d365f398b92ccc
goro-ne <hayao56@gmail.com>
2024-07-03 12:07:15 +0900

    未完了リストのボタンイベント実装

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 import './style.css'
+
+const onClickAdd = () => {
+  // テキストボックスの内容を取得して初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 対象を完了リストを移動
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    moveTarget.firstElementChild.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(moveTarget);
+  });
+  const deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click", () => {
+    // 対象を未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    console.log(deleteTarget);
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+  deleteButton.innerText = "削除";
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
